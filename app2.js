072ed6732c93730113bac47d00d332c7d379f252
João Fontanezi <94616785+Fontanezi@users.noreply.github.com>
2023-02-06 22:18:52 -0300

    Update app2.js

--- app2.js
+++ app2.js
@@ -1,16 +1,22 @@
 alert('Hello, welcome to the number guessing game! Enter a number, and the game will generate a random number between 1 and your prompt. To quit the game, type "q" at any moment.');
 
-let maxNum = prompt("Enter your number:");
+let maxNum = parseInt(prompt("Enter your number:"));
+while (!maxNum){
+    maxNum = parseInt(prompt("Enter a valid number!"));
+}
 if (maxNum === "q") {
     alert("Okay, quitting.");
-} else {
+}
+ else {
 
     const numAnswer = Math.floor(Math.random() * maxNum) + 1;
 
     let guess = prompt("Enter your first guess:");
 
     let attempts = 1;
-
+while (!parseInt(guess)){
+        guess = prompt("Enter a valid number!");
+}
     while (parseInt(guess) !== numAnswer) {
         if (guess === "q") break;
         attempts++;
@@ -26,5 +32,6 @@ if (maxNum === "q") {
         alert(`You got it! The number was ${numAnswer}. It took you ${attempts} attempts to guess.`);
     }
 }
+
 
 
